cdb34e79efce8bea814e09a4338a43db5cde67eb
Stuart Smith <stu2038@me.com>
2024-10-25 10:24:09 +1100

    Updates

---
+++ precache-manifest.587e7befd4866204568e54ee943114b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe969a12f519ed6e5f66c70bb19ab29",
+    "url": "/pricing-tool/index.html"
+  },
+  {
+    "revision": "e27ddd919577cb25bc60",
+    "url": "/pricing-tool/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "97534443a8fa598ba7e3",
+    "url": "/pricing-tool/static/css/main.6947c03c.chunk.css"
+  },
+  {
+    "revision": "e27ddd919577cb25bc60",
+    "url": "/pricing-tool/static/js/2.d4bea712.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/pricing-tool/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "97534443a8fa598ba7e3",
+    "url": "/pricing-tool/static/js/main.cffd12ea.chunk.js"
+  },
+  {
+    "revision": "173b4622b733187340f2",
+    "url": "/pricing-tool/static/js/runtime~main.b7febc52.js"
+  },
+  {
+    "revision": "68346255186cb6f79726fb64d97ae90c",
+    "url": "/pricing-tool/static/media/NDIS_logo.68346255.png"
+  }
+]);
